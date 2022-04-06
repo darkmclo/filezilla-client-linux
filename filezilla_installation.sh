ab97239d6455c3b05f3382cbb29409cf61a2acb6
Christian Alvarado <christian-alv@hotmail.com>
2022-04-06 12:18:31 -0600

    Version funcional perpetua del script

--- filezilla_installation.sh
+++ filezilla_installation.sh
@@ -3,6 +3,6 @@ clear
 
 #Descargando el paquete + Instalacion
 cd /tmp
-sudo wget -O filezilla.tar.bz2 -P /tmp "https://dl2.cdn.filezilla-project.org/client/FileZilla_3.58.0_x86_64-linux-gnu.tar.bz2?h=DdH0j54zNnQbFkZJN4NY3Q&x=1648682782" ; tar -xvjf filezilla.tar.bz2 -C /tmp ; sudo FileZilla3/bin/filezilla
+sudo wget -O filezilla.tar.bz2 -P /tmp "https://github.com/darkmclo/filezilla-client-linux/releases/download/testing/FileZilla_3.58.0_x86_64-linux-gnu.tar.bz2" ; tar -xvjf filezilla.tar.bz2 -C /tmp ; sudo FileZilla3/bin/filezilla
 
 done
